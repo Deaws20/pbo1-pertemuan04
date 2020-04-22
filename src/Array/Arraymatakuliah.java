066a79f09eb1b2ec18b999daaa8d5178a8c8e6ca
Deaws20 <d.salsabyla@gmail.com>
2020-04-22 18:55:15 +0800

    penerapan Array pada matkul

---
+++ src/Array/Arraymatakuliah.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class Arraymatakuliah {
+    public static void main(String[] args) {
+        String mataKul[];
+        mataKul = new String[10];
+
+        mataKul[0] = "Pemrograman Berbasis Objek 1";
+        mataKul[1] = "Visual 2";
+        mataKul[2] = "Rekayasa Perangkat Lunak";
+        mataKul[3] = "Organisasi Komputer";
+        mataKul[4] = "Pemrograman Web 2";
+    }
+}
