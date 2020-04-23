d652664d23964e847db5ea3f3c10319fb42314c3
Deaws20 <d.salsabyla@gmail.com>
2020-04-23 11:43:54 +0800

    Tugas Perulangan Do While

---
+++ src/Tugas/TugasPerulangan.java
@@ -0,0 +1,21 @@
+package Tugas;
+
+public class TugasPerulangan {
+    public static void main(String[] args) {
+        int saldotabungan = 200;
+        int syaratsaldoundian = 300;
+
+        System.out.println("Saldo Tabungan : " + "Rp." + saldotabungan + "jt");
+        System.out.println("Syarat Memenangkan Undian : " + "Rp." + syaratsaldoundian + "jt");
+
+        do {
+            saldotabungan++;
+            System.out.println("Tingkatkan Saldo Tabungan Anda!");
+            System.out.println("Saldo Tabungan : " + "Rp." + saldotabungan + "jt");
+        }while(saldotabungan != syaratsaldoundian);
+
+        System.out.println("Saldo Tabungan Anda Telah Mencapai " + "Rp." + syaratsaldoundian + "jt");
+        System.out.println("Anda Mendapatkan Hadiah Undian Mobil !!!");
+
+    }
+}
