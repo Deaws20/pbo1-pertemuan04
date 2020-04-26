1ecb338ccd7c1832dee75eccc07d401bdbfb76dd
Deaws20 <d.salsabyla@gmail.com>
2020-04-26 10:28:50 +0800

    Tugas Array Satu Dimensi

--- src/Tugas/TugasArray.java
+++ src/Tugas/TugasArray.java
@@ -2,11 +2,14 @@ package Tugas;
 
 public class TugasArray {
     public static void main(String[] args) {
-        String[][] bb = {{"Dea Welan Salsabyla","55Kg"}, {"Siti Bulan","58Kg"}, {"Bunga Paraswati","60Kg"}};
-        for (int i=0; i<bb.length; i++){
-            System.out.println("Nama        : " + bb[i][0]);
-            System.out.println("Berat Badan : " + bb[i][1]);
-            System.out.println("-----------------------------------");
-        }
+        String namaPeserta [];
+        namaPeserta = new String[3];
+
+        namaPeserta[0] = "Dea Welan Salsabyla";
+        namaPeserta[1] = "Siti Bulan";
+        namaPeserta[2] = "Bunga Paraswati";
+
+
     }
 }
+
